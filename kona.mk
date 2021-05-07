568a86926a5101b2b253ca8d2275cb8c44b899da
luca020400 <luca.stefani.ge1@gmail.com>
2021-05-07 20:49:31 +0200

    sm8250-common: Update Hotword Enrollment support

--- kona.mk
+++ kona.mk
@@ -214,7 +214,8 @@ PRODUCT_PACKAGES += \
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/sysconfig/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml
 
 # IPACM
 PRODUCT_PACKAGES += \
