3e1fc6af20f2a629faf8a5af4c32e71bc4cbf5fa
SebaUbuntu <barezzisebastiano@gmail.com>
2021-06-10 22:57:24 +0200

    sm8250-common: Don't override DEVICE_{MATRIX|MANIFEST}_FILE
    * Allow device-specific manifests to be safely added
    
    Change-Id: Iccafe8c8491219336389015af8caa2c7a700ff61

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,8 +75,8 @@ SOONG_CONFIG_XIAOMI_KONA_FOD := POS_X POS_Y SIZE
 TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension.kona
 
 # HIDL
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
