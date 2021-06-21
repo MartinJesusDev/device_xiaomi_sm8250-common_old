9127d81e56ffd80d6f80c9d9a8a298a2b6f7f8cb
MartinJesusDev <xdddd@23gmail.com>
2021-06-21 07:57:37 +0200

    Revert "sm8250-common: Remove unneeded rootdir files"
    This reverts commit 685c69a0f3d702c8e771597f1bb80d15d3e7e8ba.

--- kona.mk
+++ kona.mk
@@ -329,8 +329,10 @@ PRODUCT_PACKAGES += \
     init.qcom.sdio.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
     init.qti.dcvs.sh \
     init.qti.media.sh \
+    init.qti.qcv.sh \
     qca6234-service.sh
 
 PRODUCT_PACKAGES += \
@@ -338,8 +340,10 @@ PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.recovery.hardware.rc \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.target.wigig.rc \
     fstab.qcom \
     ueventd.qcom.rc
 
